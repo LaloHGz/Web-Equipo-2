daaa8535e58e26ffde4e3ce55ca466618c214d55
Emiliano Sánchez <emiliano_sanchez@criptext.com>
2022-03-14 23:13:41 -0600

    add landingAnimationsSlides

---
+++ js/animation.js
@@ -0,0 +1,14 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 4, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: .7, delay: 0.5 }); //slider
+tl.to(".intro", { y: "-100%", duration: .5 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+
+console.log("animation.js");
+
+
+
+
